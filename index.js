a9a5239dd5009eee470351f38330e932c9961c04
Miller2406 <140726735+Miller2406@users.noreply.github.com>
2023-11-07 16:29:22 +0700

    update

---
+++ index.js
@@ -0,0 +1,8 @@
+/* No need to change anything in here! */
+
+const doneTodos = document.querySelectorAll('.done');
+console.log(doneTodos);
+
+
+const checkbox = document.querySelector('input[type=checkbox]')
+console.log(checkbox);
